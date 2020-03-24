b0d1846d12bbd83a697e056cb517218c377a9acc
Alexandr767 <incognito.guitar@gmail.com>
2020-03-24 23:46:43 +0700

    xhr

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+let xhr = new XMLHttpRequest();
 var header = document.querySelector('header');
 var section = document.querySelector('section');
-var requestURL = 'package.json';
-        var request = new XMLHttpRequest();
-        request.open('GET', requestURL);
-        request.responseType = 'json';
-        request.send();
+
+xhr.open('GET', 'https://raw.githubusercontent.com/Alexandr767/Telebreeze/master/package.json');
+
+xhr.responseType = 'json';
+
+xhr.send();
+
+// the response is {"message": "Hello, world!"}
+xhr.onload = function() {
+  let site = xhr.response;
+  console.log(site.menu[0]); // Hello, world!
+};
